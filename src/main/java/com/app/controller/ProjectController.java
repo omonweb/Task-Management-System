7d43a1faf676f13ff66155b2166b6189d614eb57
Pawan Kumar Singh <pawankumarsingh3333@gmail.com>
2026-03-27 21:54:24 +0530

    feat: implement Project primary API

---
+++ src/main/java/com/app/controller/ProjectController.java
@@ -0,0 +1,27 @@
+package com.app.controller;
+
+import com.app.dto.ProjectDTO;
+import com.app.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @GetMapping
+    public List<ProjectDTO> getAllProjects(
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
+    ) {
+        return projectService.getAllProjects(startDate, endDate);
+    }
+
+}
